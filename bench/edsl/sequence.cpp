543fd5993dcb0643c541a5d340f127fe2dcfecde
Mike Rainey <mike.rainey@inria.fr>
2016-06-07 00:03:02 +0200

    Reporting elapsed time of sequential baseline calls

--- bench/edsl/sequence.cpp
+++ bench/edsl/sequence.cpp
@@ -124,7 +124,9 @@ void bench_reduce() {
   } else if (algorithm == "encore_sequential") {
     encore::launch_interpreter<sequence::reduceSerial<value_type,intT,typeof(f),typeof(g)>>(0, n, f, g, &result);
   } else if (algorithm == "sequential") {
-    result = reduceSerial<value_type>(0, n, f, g);
+    encore::run_and_report_elapsed_time([&] {
+      result = reduceSerial<value_type>(0, n, f, g);
+    });
   }
   if (check) {
 #ifndef NDEBUG
@@ -153,7 +155,9 @@ void bench_max_index() {
   } else if (algorithm == "encore_sequential") {
     encore::launch_interpreter<sequence::maxIndexSerial<value_type,intT,typeof(f),typeof(g)>>(0, n, f, g, &result);
   } else if (algorithm == "sequential") {
-    result = maxIndexSerial<value_type>(0, n, f, g);
+    encore::run_and_report_elapsed_time([&] {
+      result = maxIndexSerial<value_type>(0, n, f, g);
+    });
   }
   if (check) {
 #ifndef NDEBUG
@@ -200,7 +204,9 @@ void bench_scan() {
   } else if (algorithm == "encore_sequential") {
     encore::launch_interpreter<sequence::scanSerial<value_type,intT,typeof(f),typeof(g)>>(output, 0, n, f, g, zero, inclusive, back, &result);
   } else if (algorithm == "sequential") {
-    result = scanSerial<value_type>(output, (intT) 0, n, f, g, zero, inclusive, back);
+    encore::run_and_report_elapsed_time([&] {
+      result = scanSerial<value_type>(output, (intT) 0, n, f, g, zero, inclusive, back);
+    });
   }
   if (check) {
 #ifndef NDEBUG
@@ -236,7 +242,9 @@ void bench_pack() {
   } else if (algorithm == "encore_sequential") {
     encore::launch_interpreter<sequence::packSerial<value_type, intT, typeof(f)>>((value_type*)nullptr, flags, (intT)0, n, f, &output);
   } else if (algorithm == "sequential") {
-    output = packSerial((value_type*)nullptr, flags, (intT)0, n, f);
+    encore::run_and_report_elapsed_time([&] {
+      output = packSerial((value_type*)nullptr, flags, (intT)0, n, f);
+    });
   }
   if (check) {
 #ifndef NDEBUG
@@ -276,7 +284,9 @@ void bench_filter() {
   } else if (algorithm == "encore_sequential") {
     assert(false);
   } else if (algorithm == "sequential") {
-    output = filterSerial(input, n, p);
+    encore::run_and_report_elapsed_time([&] {
+      output = filterSerial(input, n, p);
+    });
   }
   if (check) {
 #ifndef NDEBUG
