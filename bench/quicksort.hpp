83dab1c85f627e7115f9e4361ebeb437c62931e0
Mike Rainey <bionic.oldsmobile@gmail.com>
2017-07-01 18:27:09 +0200

    improvement for encore quicksort that makes partitioning more efficient

--- bench/quicksort.hpp
+++ bench/quicksort.hpp
@@ -50,7 +50,9 @@ class quicksort : public encore::edsl::pcfg::shared_activation_record {
 public:
 
   E* A; intT n; BinPred f;
-  E* M; E* L;
+  E* M; E* L; E* R; E p; bool b;
+  using trampoline = enum { loop1, loop2 };
+  trampoline t;
 
   quicksort(E* A, intT n, BinPred f)
     : A(A), n(n), f(f) { }
@@ -61,29 +63,63 @@ public:
   dc get_dc() {
     return dc::mk_if([] (sar& s, par&) { return s.n < ISORT; }, dc::stmt([] (sar& s, par& p) {
       insertionSort2(s.A, s.n, s.f);
-    }), dc::stmts({dc::stmt([] (sar& s, par&) {
-      auto f = s.f;
+    }), dc::stmts({
+    dc::stmt([] (sar& s, par&) {
       auto n = s.n;
       auto A = s.A;
-      E p = median2(A[n/4],A[n/2],A[(3*n)/4],f);
-      E* L = A;   // below L are less than pivot
-      E* M = A;   // between L and M are equal to pivot
-      E* R = A+n-1; // above R are greater than pivot
-      while (1) {
-        while (!f(p,*M)) {
-          if (f(*M,p)) std::swap(*M,*(L++));
-          if (M >= R) break;
-          M++;
-        }
-        while (f(p,*R)) R--;
-        if (M >= R) break;
-        std::swap(*M,*R--);
-        if (f(*M,p)) std::swap(*M,*(L++));
-        M++;
-      }
-      s.L = L;
-      s.M = M;
+      s.p = median2(A[n/4],A[n/2],A[(3*n)/4],s.f);
+      s.L = A;   // below L are less than pivot
+      s.M = A;   // between L and M are equal to pivot
+      s.R = A+n-1; // above R are greater than pivot
+      s.t = loop1;
+      s.b = true;
     }),
+    dc::sequential_loop([] (sar& s, par&) { return s.b; }, dc::stmt([] (sar& s, par&) {
+      E p = s.p;
+      E* L = s.L;
+      E* M = s.M;
+      E* R = s.R;
+      auto f = s.f;
+      trampoline t = s.t;
+      int fuel = 128;
+      while (true) {
+        switch (t) {
+          case loop1: {
+            while (!f(p,*M)) {
+              if (f(*M,p)) std::swap(*M,*(L++));
+              if (M >= R) break; 
+              M++;
+              if (--fuel == 0) {
+                goto exit;
+              }
+            }
+            t = loop2;
+          }
+          case loop2: {
+            while (f(p,*R)) {
+              R--;
+              if (--fuel == 0) {
+                goto exit;
+              }
+            }
+            t = loop1;
+            if (M >= R) {
+              s.b = false;
+              goto exit;
+            }
+            std::swap(*M,*R--); 
+            if (f(*M,p)) std::swap(*M,*(L++));
+            M++;
+            --fuel;
+          }
+        }
+      }
+    exit:
+      s.t = t;
+      s.M = M;
+      s.L = L;
+      s.R = R;
+    })),
     dc::spawn2_join(
        [] (sar& s, par&, plt pt, stt st) {
          return encore_call<quicksort<E,BinPred,intT>>(st, pt, s.A, s.L-s.A, s.f);
