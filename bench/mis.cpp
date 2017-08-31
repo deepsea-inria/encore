9c541ac3b8a88afeaf8454796037af6beadf84ec
Mike Rainey <bionic.oldsmobile@gmail.com>
2017-09-01 00:00:41 +0200

    fix

--- bench/mis.cpp
+++ bench/mis.cpp
@@ -105,7 +105,7 @@ encore_pcfg_allocate(maximalIndependentSet, get_cfg)
 
 #include "sequence.h"
 #include "parallel.h"
-#include "speculative_for.h"
+#include "speculativefor.h"
 #include "gettime.h"
 //#include "loaders.hpp"
 #include "graphio.hpp"
