33b7fdb66da690564d2291f2cfe96727964b56f6
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-02-21 20:28:31 +0100

    introducing download script

---
+++ doc/download-sources.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git clone https://github.com/deepsea-inria/encore.git
+git clone https://github.com/deepsea-inria/pbbs-include.git
+git clone https://github.com/deepsea-inria/pbbs-pctl.git
+git clone https://github.com/deepsea-inria/cactus-stack.git
+git clone https://github.com/deepsea-inria/chunkedseq.git
+git clone https://github.com/deepsea-inria/cmdline.git
+git clone https://github.com/deepsea-inria/pbench.git
+git clone https://github.com/deepsea-inria/pctl.git
